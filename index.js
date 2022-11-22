e27345e7ce66a6014f75eb2445a26233ef418bde
Avinash Ramsumair <avi@pure235games.com>
2022-11-22 09:52:51 -0500

    commit tree f528f78f4b920920abec38dad3adc3814a1c82da

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distanceLocation){
+    return Math.abs(42 - distanceLocation);
+}
+
+function  distanceFromHqInFeet(distanceLocation){
+    return (264 *  distanceFromHqInBlocks(distanceLocation));
+}
+
+function distanceTravelledInFeet (startLocation, endLocation){
+    return (264 * (Math.abs(startLocation - endLocation)));
+}
+
+function calculatesFarePrice(startLocation, endLocation){
+    const distanceTravelled = distanceTravelledInFeet(startLocation, endLocation);
+    if (distanceTravelled > 2500){
+        return 'cannot travel that far';
+    }
+    else if (distanceTravelled > 2000){
+        return 25;
+    }
+    else if (distanceTravelled > 400){
+        return ((.02 * (distanceTravelled - 400)));
+    }
+    else {
+        return 0;
+    }
+}
